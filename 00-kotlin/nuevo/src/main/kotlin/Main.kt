8af5853cec18a4a3ce6b523d54419f7e1a02f8f3
Jenny Llano <jenny.llano@epn.edu.ec>
2022-12-07 12:59:09 -0500

    introducción 2.0 kotlin

--- 00-kotlin/nuevo/src/main/kotlin/Main.kt
+++ 00-kotlin/nuevo/src/main/kotlin/Main.kt
@@ -21,7 +21,7 @@ fun main(){
     //variables primitivas
     val nombreProfesor: String = "Adrian"
     val sueldo: Double = 1.2
-    val estadoCivil: char = 'S'
+    val estadoCivil: Char = 'S'
     val mayorEdad: Boolean = true
     //Clases JAVA
     val fechaNacimiento: Date = Date()
@@ -46,6 +46,17 @@ fun main(){
     }
     val coqueteo = if (estadoCivilWhen == "S")"SI" else "NO"
 
+    ////////////////
+    var sumaUno = Suma(1,2);
+    var sumaDos = Suma(1, null);
+    var sumaTres = Suma(null, 1);
+    sumaUno.sumar()
+    sumaDos.sumar()
+    Suma.pi
+    Suma.historialSumas
+    Suma.elevarAlCuadrado(2)
+
+
 }
 
 //voidd == Unit
@@ -68,6 +79,80 @@ fun calcularSueldo(
     }
 }
 
+abstract class NumerosJava{
+    protected val numeroUno: Int
+    private val numeroDos: Int
+
+    constructor(
+        uno: Int,
+        dos: Int
+    ){//Bloque código constructor
+        this.numeroUno = uno
+        this.numeroDos = dos
+        println("Inicializado")
+    }
+}
+
+abstract class Numeros( //Constructor Primario
+    //uno: Int, //parámetro
+    protected val numeroUno: Int, //propiedad de la clase protected
+    protected val numeroDos: Int //propiedad de la clase protected
+    ){
+        init { //Bloque de código constructor PRIMARIO
+            this.numeroUno
+            numeroUno
+            this.numeroDos
+            numeroDos
+            println("inicializado")
+        }
+
+}
+
+class Suma(//constructor primario suma
+    uno: Int, //parámetro
+    dos: Int //parámetro
+): Numeros(uno, dos){
+    init{ //bloque constructor primario
+        this.numeroUno
+        this.numeroDos
+    }
+    constructor(//segundo constructor
+        uno: Int?, //parametros
+        dos: Int //parametro
+    ):this( //llamada al constructor primario
+        if(uno ==null) 0 else uno,
+        dos
+    ){} //Bloque de código del constructor
+    constructor(//tercer constructor
+        uno: Int, //parametros
+        dos: Int? //parametro
+    ):this( //llamada al constructor primario
+        uno,
+        if(dos ==null) 0 else uno
+    ){}
+    constructor(//cuarto constructor
+        uno: Int?, //parametros
+        dos: Int? //parametro
+    ):this( //llamada al constructor primario
+        if(uno ==null) 0 else uno,
+        if(dos ==null) 0 else uno
+    )
+    public fun sumar():Int{
+        return numeroUno+numeroDos
+    }
+
+    companion object{// atributos y métodos "compartidos" entre las instancias
+        val pi=3.14
+        fun elevarAlCuadrado(num:Int):Int{
+            return num*num
+        }
+        val historialSumas = arrayListOf<Int>()
+        fun agregarHistorial(valorNuevaSuma: Int){
+            historialSumas.add(valorNuevaSuma)
+        }
+
+    }
+}
 
 
 
